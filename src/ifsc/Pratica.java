54aae76aa9eafeb1c1aa7b73f763f675526b2e8c
Bárbara <barbara.n2005@aluno.ifsc.edu.br>
2022-08-22 17:31:16 -0300

    lista 01

---
+++ src/ifsc/Pratica.java
@@ -0,0 +1,22 @@
+package ifsc;
+
+public class Pratica {
+	
+	public static void main(String[] args) {
+		
+		int aux = 0;
+		while (aux <= 5);
+		     System.out.println("olá");
+		     aux++;
+		}
+	
+    }
+ public Double calculaAbastecimento(Double qtdLitros, Double precoLitro) {
+	 if(qtdLitros == null) {
+		 
+	 }
+	 
+	
+	return qntLitros* precoLitro;
+  }
+ }
